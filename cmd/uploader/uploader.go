9eccc78d02574240f44b1de90285ff11d87b0e73
Archs <gitarchs@yeah.net>
2015-09-16 06:22:14 +0800

    refine uploadDirectory

--- cmd/uploader/uploader.go
+++ cmd/uploader/uploader.go
@@ -55,16 +55,19 @@ func uploadDirectory(dirPath string) error {
 		log.Println(dirPath, "is a directory")
 		return nil
 	}
-	log.Println("Uploading directory:", dirPath, "...")
 	walkFn := func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			log.Println("Walking into", path, "error:", err)
 			return err
 		}
 		// for dir
 		if info.IsDir() {
+			if path == dirPath {
+				return nil
+			}
 			err = uploadDirectory(path)
-			log.Printf("Uploading dir:%s error:%s", path, err.Error())
+			if err != nil {
+				log.Printf("Uploading dir:%s error:%s", path, err.Error())
+			}
 			return nil
 		}
 		// for file
@@ -73,17 +76,12 @@ func uploadDirectory(dirPath string) error {
 			log.Printf("Uploading file:%s error:%s", path, err.Error())
 		}
 		return nil
-
 	}
+	log.Println("Uploading directory:", dirPath, "...")
 	return filepath.Walk(dirPath, walkFn)
 }
 
 func main() {
-	flag.StringVar(&server, "server", "http://localhost:9333", `SeaweedFS master location`)
-	flag.StringVar(&collection, "col", "", `optional collection name`)
-	flag.StringVar(&replication, "replication", "", "replication type")
-	flag.BoolVar(&debug, "debug", false, "verbose debug information")
-	flag.BoolVar(&recursive, "r", false, `upload directory recursivly (default false)`)
 	flag.Parse()
 	client = weedo.NewClient(server)
 	if err := client.Master().Status(); err != nil {
@@ -114,4 +112,14 @@ func main() {
 	for fid, fpath := range fmap {
 		println(fid, "\t", fpath)
 	}
+}
+
+func init() {
+	flag.StringVar(&server, "server", "http://localhost:9333", `SeaweedFS master location`)
+	flag.StringVar(&collection, "col", "", `optional collection name`)
+	flag.StringVar(&replication, "replication", "", "replication type")
+	flag.BoolVar(&debug, "debug", false, "verbose debug information")
+	flag.BoolVar(&recursive, "r", false, `upload directory recursivly (default false)`)
+	// log opt
+	log.SetFlags(log.Ldate | log.Ltime)
 }
