15330fd2b513d45225534c7d1fd11df503e203f2
Archs <gitarchs@yeah.net>
2015-09-16 04:14:55 +0800

    ok fid

--- fidcookies/fid.go
+++ fidcookies/fid.go
@@ -1,9 +1,13 @@
 package fidcookies
 
 import (
+	"errors"
+	"fmt"
 	"mime"
 	"os"
 	"path"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -20,6 +24,23 @@ type Fid struct {
 	Cookie uint32 // cookie
 }
 
+func NewFid(volId, fullPath string) (*Fid, error) {
+	fid := new(Fid)
+	if id, err := strconv.ParseUint(volId, 10, 32); err != nil {
+		return nil, err
+	} else {
+		fid.Id = uint32(id)
+	}
+	if err := fid.InsertKeyAndCookie(fullPath); err != nil {
+		return nil, err
+	}
+	return fid, nil
+}
+
+func (f *Fid) VolumeID() string {
+	return strconv.Itoa(int(f.Id))
+}
+
 // Fid in string form
 func (f *Fid) String() string {
 	return fmt.Sprintf("%d,%x%8x", f.Id, f.Key, f.Cookie)
@@ -28,16 +49,17 @@ func (f *Fid) String() string {
 // Set Fid.Key to current nano seconds since 1970's
 // which is monotonouse increased :)
 // Let's hope this key would not collides with each other
-func (f *Fid) SetTimeKey() {
-	f.Key = time.Now().UnixNano()
+func (f *Fid) insertKey() {
+	f.Key = uint64(time.Now().UnixNano())
 }
 
 // Set Fid.Cookie(32 bits) according to the file infomation
 // Fid.Cookie contains the mime type info and file size(in KB)
-// midx takes the left 10 bits
-// size tekes the rigth 22 bits
-func (f *Fid) SetCookie(fullPath string) error {
-	mtype := mime.TypeByExtension(path.Ext(fullPathm))
+// midx takes the left 10 bits   mask: 0xffc00000
+// size tekes the rigth 22 bits  mask: 0x003fffff
+func (f *Fid) insertCookie(fullPath string) error {
+	// cookie
+	mtype := mime.TypeByExtension(path.Ext(fullPath))
 	idx, ok := mmap[mtype]
 	if !ok {
 		idx = mmap[defaultMimeType]
@@ -55,21 +77,49 @@ func (f *Fid) SetCookie(fullPath string) error {
 	return nil
 }
 
-func ParseFid(fid string) (*Fid, error) {
+func (f *Fid) InsertKeyAndCookie(fullPath string) error {
+	f.insertKey()
+	return f.insertCookie(fullPath)
+}
+
+func (f *Fid) Time() time.Time {
+	return time.Unix(0, int64(f.Key))
+}
+
+// mime type for this fid
+func (f *Fid) MimeType() string {
+	midx := f.Cookie & 0xffc00000
+	midx = midx >> 22
+	return mslice[midx]
+}
+
+// Size in KB
+func (f *Fid) Size() int {
+	return int(f.Cookie & 0x003fffff)
+}
+
+func ParseFid(s string) (*Fid, error) {
+	fid := new(Fid)
 	a := strings.Split(s, ",")
 	if len(a) != 2 || len(a[1]) <= 8 {
-		return fid, errors.New("Fid format invalid")
+		return nil, errors.New("Fid format invalid")
 	}
-	if fid.Id, err = strconv.ParseUint(a[0], 10, 32); err != nil {
-		return
+	// id
+	id, err := strconv.ParseUint(a[0], 10, 32)
+	if err != nil {
+		return nil, err
 	}
+	fid.Id = uint32(id)
+	// key
 	index := len(a[1]) - 8
 	if fid.Key, err = strconv.ParseUint(a[1][:index], 16, 64); err != nil {
-		return
+		return nil, err
 	}
-	if fid.Cookie, err = strconv.ParseUint(a[1][index:], 16, 32); err != nil {
-		return
+	// cookie
+	if cookie, err := strconv.ParseUint(a[1][index:], 16, 32); err != nil {
+		return nil, err
+	} else {
+		fid.Cookie = uint32(cookie)
 	}
-
-	return
+	return fid, nil
 }
